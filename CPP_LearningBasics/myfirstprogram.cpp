0c33b7ad1ddc3bf700ea14aad75ad3cf1a7ab639
JoseLRC224 <joselurc24@gmail.com>
2026-01-10 01:18:58 -0600

    Agregados ejercicios de conceptos básicos y saltos de línea

---
+++ CPP_LearningBasics/myfirstprogram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!!!" << endl;
+    
+    cout << "I'm learning c++ again" << endl;
+
+    return 0;
+}
---
+++ CPP_LearningBasics/numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //maneras de hacer salto de linea
+    cout << 3 << endl;
+    cout << "salto de linea" << "\n\n";
+    cout << 12 * 2 + 200 << endl;
+
+
+
+    //haciendo tabs
+    cout << "crearemos una tabulacion \t";
+    cout << "tab" << endl;
+
+    //lear how to use "\n"
+    cout << "test de como hacer salto de linea\n";
+    cout << "funcionó \n";
+
+    //usando de separador el caracter 
+    cout << "separar 1\\ ";
+    cout << "2";
+    cout << endl;
+    return 0;
+}
